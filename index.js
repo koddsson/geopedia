8e9193be1d11e8824e05dac7d9de9107eec7140d
Kristján Oddsson <koddsson@gmail.com>
2022-09-22 10:44:22 +0200

    Just speak the entry once

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ async function findPagesNear(latitude, longitude) {
     action: 'query',
     list: 'geosearch',
     origin: '*',
-    gscoord: `${latitude}|-${longitude}`,
+    gscoord: `${latitude}|${longitude}`,
     gsradius: 10000,
     gslimit: 1,
     format: 'json'
@@ -22,19 +22,54 @@ async function findPagesNear(latitude, longitude) {
 
   const headers = new Headers({Accept: 'application/json'})
   const response = await fetch(url, {headers})
-  console.log(response)
   const json = await response.json()
 
   return json.query.geosearch
 }
 
-setInterval(async function() {
-  console.log('=======>')
+async function getPageText(pageid) {
+  const url = new URL('https://en.wikipedia.org/w/api.php')
+  url.search = new URLSearchParams({
+    action: 'query',
+    prop: 'extracts',
+    exintro: true,
+    explaintext: true,
+    pageids: pageid,
+    origin: '*',
+    format: 'json'
+  }).toString()
+  
+  const headers = new Headers({Accept: 'application/json'})
+  const response = await fetch(url, {headers})
+  const json = await response.json()
+  return json.query.pages[pageid].extract
+}
+
+function speak(text) {
+  const synth = window.speechSynthesis;
+  
+  const utterThis = new SpeechSynthesisUtterance(text);
+  utterThis.voice = synth.getVoices().find(x => x.lang === 'en-scotland')
+  synth.speak(utterThis);
+}
+
+(async function() {
   const {coords: {latitude, longitude}} = await getCurrentPosition()
   const locations = await findPagesNear(latitude, longitude)
-  for (const location of locations) {
-    const el = document.createElement('div')
-    el.innerText = location
-    document.body.append(el)
+  if (locations.length === 0) {
+    console.log('No items')
   }
-}, 10000)
+  for (const location of locations) {
+    const text = await getPageText(location.pageid)
+    const dl = document.createElement('dl')
+    const dt = document.createElement('dt')
+    dt.textContent = location.title
+    const dd = document.createElement('dd')
+    dd.textContent = text
+    dl.append(dt, dd)
+  
+    speak(text)
+  
+    document.body.append(dl)
+  }
+})()
