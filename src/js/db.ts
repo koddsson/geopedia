9dedbc20bd84fc58cf55147706b245db4d1a6f66
Kristján Oddsson <koddsson@gmail.com>
2022-11-26 14:24:31 +0100

    Create a new `.get` method for database and rename `.get` to `.getAll`

--- src/js/db.ts
+++ src/js/db.ts
@@ -6,13 +6,19 @@ export class Database<T extends DatabaseEntry> {
   constructor(public key: string) {}
 
   save(datum: T) {
-    const existingData = this.get<T>()
+    const existingData = this.getAll()
     existingData.push(datum)
     
     localStorage.setItem(this.key, JSON.stringify(existingData))
   }
+
+  get(id: string): T | undefined {
+    const all = this.getAll()
+    return all.find(item => item.id === id)
+  }
   
-  get<T>(): T[] {
+  getAll(): T[] {
     return JSON.parse(localStorage.getItem(this.key) || '[]')
   }
+
 }
