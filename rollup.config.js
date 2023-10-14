7f44209cd35aa90234041e6c0c96c36380c8f483
Kristján Oddsson <koddsson@gmail.com>
2023-10-14 16:05:53 +0200

    add target to esbuild options

--- rollup.config.js
+++ rollup.config.js
@@ -4,5 +4,5 @@ import esbuild from 'rollup-plugin-esbuild'
 export default {
   input: 'index.html',
   output: {dir: 'dist'},
-  plugins: [esbuild(), html({input: 'index.html'})]
+  plugins: [esbuild({target: 'es2022'}), html({input: 'index.html'})]
 }
